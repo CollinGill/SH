b9d702f34045c2889e3546c940c5dc69c076eaf3
CollinGill <collingill02@gmail.com>
2022-10-01 21:48:31 -0400

    login better

--- client/src/login/index.js
+++ client/src/login/index.js
@@ -1,89 +1,75 @@
-import React, { useState } from "react";
+import React from "react";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import Key from "@mui/icons-material/Key";
 
+import Navbar from "../components/Navbar";
+import { Box } from "@mui/system";
 import {
-  Box,
   Button,
   FormControl,
   Grid,
-  InputLabel,
   Input,
   InputAdornment,
-  Snackbar,
+  InputLabel,
 } from "@mui/material";
 
-import Navbar from "../components/Navbar";
-
 const Login = () => {
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const errors = {
-    uname: "Invalid username",
-    pass: "Invalid password",
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // TODO: Login authorization with Spotify and send user to home page with their stats?
-    // TODO: Clear form
-  };
-
-  const renderErrorMsg = (name) =>
-    name === errorMessages.name && (
-      <Snackbar autoHideDuration={6000} message={errorMessages.message} />
-    );
-
-  const itemSize = 10;
-
   return (
     <div>
       <Navbar />
-      <form onSubmit={handleSubmit}>
-        <Grid
-          container
-          spacing="16"
-          alignItems="center"
-          justify="center"
-          direction="column"
-          backgroundColor="#616161"
-          padding={"5em"}
-          sx={({ width: 1 / 2 }, { height: 1 / 2 })}
-        >
-          <Grid item xs={itemSize}>
-            <FormControl variant="standard">
-              <InputLabel>Username/Email</InputLabel>
-              <Input
-                startAdornment={
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
+      <Box height={100} />
+      <Box
+        backgroundColor={"#616161"}
+        border={2}
+        borderColor={"black"}
+        borderRadius={16}
+        width={400}
+        height={500}
+        margin={"auto"}
+      >
+        <form>
+          <Grid
+            container
+            spacing={3}
+            paddingLeft={10}
+            paddingTop={10}
+            direction={"column"}
+          >
+            <Grid item xs>
+              <FormControl>
+                <InputLabel>Username / Email</InputLabel>
+                <Input
+                  id={"uname"}
+                  startAdornment={
+                    <InputAdornment position={"start"}>
+                      <AccountCircle />
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs>
+              <FormControl>
+                <InputLabel>Password</InputLabel>
+                <Input
+                  id={"uname"}
+                  type={"password"}
+                  startAdornment={
+                    <InputAdornment position={"start"}>
+                      <Key />
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs>
+              <Button type="submit" variant="contained">
+                Login
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={itemSize}>
-            <FormControl variant="standard">
-              <InputLabel>Password</InputLabel>
-              <Input
-                type="password"
-                startAdornment={
-                  <InputAdornment position="start">
-                    <Key />
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </Grid>
-          <Grid item xs={itemSize}>
-            <Button variant="contained" color="primary" type="submit">
-              Login
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+        </form>
+      </Box>
     </div>
   );
 };
